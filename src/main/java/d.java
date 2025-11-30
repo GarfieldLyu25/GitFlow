09769efd5e7e220a4a11b4baeb9cee9c8c442e8e
Garfield Lyu <lv2895045412@gmail.com>
2025-11-30 21:28:16 +0800

    模拟冲突

--- src/main/java/d.java
+++ src/main/java/d.java
@@ -5,6 +5,6 @@ public class d {
         System.out.println("这是d需求和c同步开发");
         System.out.println("reset push");
         System.out.println("正在开发 半成品....开发完e完成");
-        System.out.println("用户a的提交");
+        System.out.println("本地写的冲突");
     }
 }
